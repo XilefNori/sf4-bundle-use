0ff1fe9593a73e10182d0dbeb9b5f2330462e8f2
sboborykin <sergey@boborykin.ru>
2020-02-18 20:17:42 +0300

    ~ fix bundle alias and make it read config

--- lib/LoremIpsumBundle/src/KnpULoremIpsumBundle.php
+++ lib/LoremIpsumBundle/src/KnpULoremIpsumBundle.php
@@ -2,9 +2,19 @@
 
 namespace KnpU\LoremIpsumBundle;
 
+use KnpU\LoremIpsumBundle\DependencyInjection\KnpULoremIpsumExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KnpULoremIpsumBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        if (null === $this->extension) {
+            $this->extension = new KnpULoremIpsumExtension;
+        }
 
+        if ($this->extension) {
+            return $this->extension;
+        }
+    }
 }
