a480a1074400ca50f0dccb8549eb04f011d7737c
sboborykin <sergey@boborykin.ru>
2020-03-13 20:30:59 +0300

    + add CustomWordProvider to tagged services

--- src/Service/CustomWordProvider.php
+++ src/Service/CustomWordProvider.php
@@ -9,31 +9,6 @@ class CustomWordProvider implements WordProviderInterface
     public function getWordList(): array
     {
         return [
-            'sunshine',
-            'rainbows',
-            'unicorns',
-            'puns',
-            'butterflies',
-            'cupcakes',
-            'sprinkles',
-            'glitter',
-            'friend',
-            'high-five',
-            'friendship',
-            'compliments',
-            'sunsets',
-            'cookies',
-            'flowers',
-            'bikes',
-            'kittens',
-            'puppies',
-            'macaroni',
-            'freckles',
-            'baguettes',
-            'presents',
-            'fireworks',
-            'chocholate',
-            'marshmallow',
             'beach',
         ];
     }
